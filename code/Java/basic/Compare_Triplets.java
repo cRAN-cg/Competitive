f8b88e23b088917f16663f6c0469011e527edfc1
cRAN-cg <guptachiranjeev.13@gmail.com>
2017-06-02 11:04:58 +0530

    Add solution to \'Compare Triplets\' in Java

---
+++ code/Java/basic/Compare_Triplets.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+
+public class Compare_Triplets {
+
+	static int[] solve( int a0, int a1, int a2, int b0, int b1, int b2){
+		// Complete this function
+		int [] result = {(a0>b0?1:0) + (a1>b1?1:0) + (a2>b2?1:0) , (a0<b0?1:0) + (a1<b1?1:0) + (a2<b2?1:0)};
+		return result;
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int a0 = in.nextInt();
+		int a1 = in.nextInt();
+		int a2 = in.nextInt();
+		int b0 = in.nextInt();
+		int b1 = in.nextInt();
+		int b2 = in.nextInt();	
+
+		int [] result =  solve(a0, a1, a2, b0, b1, b2);
+		for (int i=0; i< result.length(); i++){
+			System.out.println(result[i] + (i! = result.length()-1? " " : ""));
+		}
+	}
+
+
+}
