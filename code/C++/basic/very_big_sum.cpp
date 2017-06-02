7e407262780edd62b18515dfaac65966b92f095e
cRAN-cg <guptachiranjeev.13@gmail.com>
2017-06-02 14:19:15 +0530

    Update solution to 'A Very Big Sum' https://www.hackerrank.com/challenges/a-very-big-sum.

--- code/C++/basic/very_big_sum.cpp
+++ code/C++/basic/very_big_sum.cpp
@@ -1,25 +1,29 @@
-#include <bits/stdc++.h>
-
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 typedef long long ll;
-int main(int argc, char const *argv[])
-{
-	
-	int n;
-	cin >> n;
-	vector<ll> v(n);
-	for(int arr_i = 0; arr_i < n; arr_i++){
-		cin >> v[arr_i];
-	}
-	ll result = solution(v);
-	cout << result ;
-	return 0;
+
+ll solve(vector<ll> v, int length){
+  ll result = 0;
+  for (int n = length-1; n >= 0; n--){
+    result += v[n];
+  }
+  return result;
 }
 
-ll solution (vector<ll> v){
-	ll result;
-	for ( int n = v.size - 1; n >= 0; n-- ){
-		result += v[n];
-	}
-	return result;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<ll> arr(n);
+    for(int arr_i = 0;arr_i < n;arr_i++){
+       cin >> arr[arr_i];
+    }
+    ll result =  solve(arr, n);
+    cout << result ;
+    return 0;
 }
+
