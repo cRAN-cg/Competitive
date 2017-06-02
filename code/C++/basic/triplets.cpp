cf9b06ed7056fd2f5205afef91111ab6acddd796
cRAN-cg <guptachiranjeev.13@gmail.com>
2017-06-02 10:30:54 +0530

    Solution to \'Compare the Triplets\' prblem https://www.hackerrank.com/challenges/compare-the-triplets

---
+++ code/C++/basic/triplets.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector < int > solve(int a0, int a1, int a2, int b0, int b1, int b2){
+    vector <int> people = {0, 0};
+    people[0] = (a0 > b0 ? 1 : 0) + (a1 > b1 ? 1 : 0) + (a2 > b2 ? 1 : 0);
+    people[1] = (a0 < b0 ? 1 : 0) + (a1 < b1 ? 1 : 0) + (a2 < b2 ? 1 : 0); 
+    
+    return people;
+}
+
+int main() {
+    int a0;
+    int a1;
+    int a2;
+    cin >> a0 >> a1 >> a2;
+    int b0;
+    int b1;
+    int b2;
+    cin >> b0 >> b1 >> b2;
+    vector < int > result = solve(a0, a1, a2, b0, b1, b2);
+    for (ssize_t i = 0; i < result.size(); i++) {
+        cout << result[i] << (i != result.size() - 1 ? " " : "");
+    }
+    cout << endl;
+    
+
+    return 0;
+}
